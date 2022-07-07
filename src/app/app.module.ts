d59b3b376c3361bd9c808c9f1b9f9c19292fcd5a
Marcello Caccamo <londonliving89@gmail.com>
2022-07-07 21:14:12 +1000

    more components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
-<<<<<<< HEAD
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { AboutComponent } from './components/about/about.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -19,9 +18,6 @@ const appRoutes: Routes = [
   { path: '', component: TasksComponent },
   { path: 'about', component: AboutComponent },
 ];
-=======
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
->>>>>>> ecdb19a03251e23d960e6e066a08bf2c0580c5e4
 
 @NgModule({
   declarations: [
@@ -29,7 +25,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-<<<<<<< HEAD
     TaskItemComponent,
     AddTaskComponent,
     AboutComponent,
@@ -41,13 +36,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot(appRoutes, { enableTracing: true }),
-=======
-    TaskItemComponent
-  ],
-  imports: [
-    BrowserModule,
-    FontAwesomeModule
->>>>>>> ecdb19a03251e23d960e6e066a08bf2c0580c5e4
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/services/task.service.spec.ts
+++ src/app/services/task.service.spec.ts
@@ -13,8 +13,4 @@ describe('TaskService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> ecdb19a03251e23d960e6e066a08bf2c0580c5e4
