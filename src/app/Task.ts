53724ffec41ef9269c34231bd5db60ce74ed93ee
Marcello Caccamo <londonliving89@gmail.com>
2022-07-13 21:02:18 +1000

    Delete buttons logic

--- src/app/Task.ts
+++ src/app/Task.ts
@@ -3,8 +3,4 @@ export interface Task {
     text: string;
     day: string;
     reminder: boolean;
-<<<<<<< HEAD
   }
-=======
-}
->>>>>>> ecdb19a03251e23d960e6e066a08bf2c0580c5e4
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
-import { AddTaskComponent } from './components/add-task/add-task.component';
-import { AboutComponent } from './components/about/about.component';
-import { FooterComponent } from './components/footer/footer.component';
+// import { AddTaskComponent } from './components/add-task/add-task.component';
+// import { AboutComponent } from './components/about/about.component';
+// import { FooterComponent } from './components/footer/footer.component';
 
 const appRoutes: Routes = [
   { path: '', component: TasksComponent },
-  { path: 'about', component: AboutComponent },
+  // { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
@@ -26,9 +26,9 @@ const appRoutes: Routes = [
     ButtonComponent,
     TasksComponent,
     TaskItemComponent,
-    AddTaskComponent,
-    AboutComponent,
-    FooterComponent,
+    // AddTaskComponent,
+    // AboutComponent,
+    // FooterComponent,
   ],
   imports: [
     BrowserModule,
